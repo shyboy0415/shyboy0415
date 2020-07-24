d130a8d80d4f94650105c3cc9b140d7f6c30c5d3
shyboy0415 <61101714+shyboy0415@users.noreply.github.com>
2020-07-24 16:55:57 +0300

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -10,20 +10,15 @@ from bs4 import BeautifulSoup
 
 class Crawler:
     domain = ""
-    #url_root = 'https://91biquge.cc'
-    #url_mine_list = [
-    #    'https://91biquge.cc',
-    #]
-    
-    file = None
-    output_file = ""
+
+    output_file = None
 
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
 
-    url_res_final = []
+    url_res_final = set([])
     max_depth = 2
     current_depth = 1
     times = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S+08:00")
@@ -43,14 +38,15 @@ class Crawler:
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
     DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 
-    def __init__(self, domain="", output="sitemap.xml", depth=1):
+    def __init__(self, domain = "", output = "sitemap.xml", depth = 2):
         self.domain = domain
-        self.output_file = output
+        self.output_file = open(output, 'w', encoding='utf-8')
         self.max_depth = depth
         logging.basicConfig(filename='sitemap_generator.log', level=logging.INFO, format=self.LOG_FORMAT, datefmt=self.DATE_FORMAT)
         logging.debug('domain: %s', self.domain)
-        logging.debug('output: %s', self.output_file)
+        logging.debug('output: %s', output)
         logging.debug('depth: %s', self.max_depth)
+        self.url_res_final.add(self.domain)
         if self.max_depth <= 0 or self.max_depth > 3:
             raise IllegalArgumentError("Depth must be 1 - 3")
 
@@ -76,15 +72,14 @@ class Crawler:
         return self.filterlinks(links)
 
     def filterlinks(self, links):
-        tmplinks = []
+        tmplinks = set([])
         for link in links:
             url = str(link.get('href'))
             if url is None or url == '':
                 continue
             ishttp = url.startswith('http')
-            ismine = url.startswith(self.domain)
-            if ishttp and (not ismine):
-                continue
+            #if ishttp and (not ismine):
+            #    continue
             if url.startswith('#') or '/' == url.strip():
                 continue
             if url.startswith("?"):
@@ -97,19 +92,24 @@ class Crawler:
                 url = self.domain  + url
             elif not ishttp:
                 url = self.domain + '/'  + url
-            tmplinks.append(url)
-        reslinks = list(set(tmplinks))
+            tmplinks.add(url)
+            
+        reslinks = list(tmplinks)
         return reslinks
 
     def parser(self, url_arr, depth):
         logging.debug('sitemap depth %s start', depth)
         url_tmp = []
         for urlmine in url_arr:
+            ismine = urlmine.startswith(self.domain)
+            if not ismine:
+                continue
             links = self.getlinks(urlmine)
             url_tmp.extend(links)
             logging.debug('processing: %s get %s urls', urlmine, len(links))
-        url_tmp = list(set(url_tmp).difference(set(self.url_res_final)))
-        self.url_res_final.extend(url_tmp)
+        url_tmp = list(set(url_tmp).difference(self.url_res_final))
+        logging.debug('get %s for next depth', len(url_tmp))
+        self.url_res_final.update(set(url_tmp))
         self.sitemap_add_urls(url_tmp, depth)
         depth += 1
         if(depth > self.max_depth):
@@ -118,15 +118,12 @@ class Crawler:
 
     def sitemap_header(self):
         header = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-        self.file = open(self.output_file, 'w', encoding='utf-8')
-        self.file.writelines(header)
-        
+        self.output_file.writelines(header)
         url = self.domain
         urls = re.sub(r"&", "&amp;", url)
         ment = "  <url>\n    <loc>%s</loc>\n    <lastmod>%s</lastmod>\n    <changefreq>daily</changefreq>\n    <priority>1.00</priority>\n  </url>\n" % (urls, self.times)
-        self.file.writelines(ment)
-        self.file.flush
-        #self.file.close()
+        self.output_file.writelines(ment)
+        self.output_file.flush
         
     def sitemap_add_urls(self, url_list, depth):
         priority = self.get_priority(depth)
@@ -135,15 +132,15 @@ class Crawler:
         for url in url_list:
             urls = re.sub(r"&", "&amp;", url)
             ment = "  <url>\n    <loc>%s</loc>\n    <lastmod>%s</lastmod>\n    <changefreq>%s</changefreq>\n    <priority>%s</priority>\n  </url>\n" % (urls, self.times, frequency,priority)
-            logging.debug('add url into xml: %s \n', ment)
-            self.file.writelines(ment)
-        self.file.flush
+            #logging.debug('add url into xml: %s \n', ment)
+            self.output_file.writelines(ment)
+        self.output_file.flush
         
     def sitemap_end(self):
         last = "</urlset>"
-        self.file.writelines(last)
-        self.file.flush
-        self.file.close()
+        self.output_file.writelines(last)
+        self.output_file.flush
+        self.output_file.close()
         
     def get_priority(self, depth):
         priority = '0'
@@ -165,14 +162,13 @@ class Crawler:
         
     def run(self):
         starttime = datetime.datetime.now()
-        self.times = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S+08:00")
-        url_tmp = []
-        url_tmp.append(self.domain)
-        self.url_res_final.append(self.domain)
-        logging.debug('parser domain: %s', url_tmp)
+        self.times = starttime.strftime("%Y-%m-%dT%H:%M:%S+08:00")
+        logging.debug('parser domain: %s', self.domain)
         self.sitemap_header()
-        self.parser(url_tmp, 1)
+        self.parser(list(self.url_res_final), 1)
         self.sitemap_end()
         endtime = datetime.datetime.now()
-        logging.debug('sitemap generate take: %s s', (endtime - starttime).seconds)
-        logging.debug('sitemap size is %s urls', len(self.url_res_final))
+        logging.info('======Report of Sitemap Generator ======')
+        logging.info('generate sitemap for %s', self.domain)
+        logging.info('generate sitemap take: %s s', (endtime - starttime).seconds)
+        logging.info('sitemap include %s urls', len(self.url_res_final))
